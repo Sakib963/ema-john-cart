984c5ee5a4a595094b32a57e5837a7cb045869ef
Sakib963 <abdullah15-963@diu.edu.bd>
2023-03-28 16:52:56 +0600

    Show data from cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -15,15 +15,42 @@ const Shop = () => {
     },[])
 
     useEffect(() => {
+        const savedCart = [];
         const storedCart = getShoppingCart();
-        console.log(storedCart)
-    }, [])
+        // Step 1: get id
+        for(const id in storedCart){
+            // Step 2: Get the product by product id
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct){
+                // Step 3: Get quantity of the product
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // Step 4: Add the product to the saved Cart
+                savedCart.push(addedProduct);
+            }
+            console.log(addedProduct)
+        }
+        // Step 5: Set the Cart 
+        setCart(savedCart)
+    }, [products])
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        let newCart = [];
+        // If product doesn't exists in the cart, then set quantity = 1
+        // else update the quantity by 1
+        const exists = cart.find((item => item.id === product.id));
+        if(!exists){
+            product.quantity = 1;
+            newCart= [...cart, product]
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
+
         setCart(newCart);
         addToDb(product.id)
-        console.log(newCart)
       }
 
     return (
